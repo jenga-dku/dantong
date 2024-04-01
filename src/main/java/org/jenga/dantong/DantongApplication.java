867095554c66937f9cfc1ecb1453813e7bfca199
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-04-01 20:55:15 +0900

    Dantong back-end initialize

---
+++ src/main/java/org/jenga/dantong/DantongApplication.java
@@ -0,0 +1,13 @@
+package org.jenga.dantong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DantongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DantongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jenga/dantong/DantongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jenga.dantong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DantongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
