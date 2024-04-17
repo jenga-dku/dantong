217b1da09bcf12b58ce64523915b8a45bb21a1eb
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-04-17 19:42:25 +0900

    feat: 설문 관련 클래스 엔티티로 변경

--- src/main/java/org/jenga/dantong/survey/model/entity/SurveyItem.java
+++ src/main/java/org/jenga/dantong/survey/model/entity/SurveyItem.java
@@ -1,7 +1,14 @@
 package org.jenga.dantong.survey.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class SurveyItem {
 
+    @Id
+    @GeneratedValue
     private int surveyItemId;
     private Tag tag;
     private int surveyId;
--- src/main/java/org/jenga/dantong/survey/model/entity/SurveyReply.java
+++ src/main/java/org/jenga/dantong/survey/model/entity/SurveyReply.java
@@ -1,7 +1,14 @@
 package org.jenga.dantong.survey.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class SurveyReply {
 
+    @Id
+    @GeneratedValue
     private int id;
     private int surveyItemId;
     private String content;
