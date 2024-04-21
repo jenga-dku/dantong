73470386437c22c6fd8206bf20ae399a9f32a20a
Sermadl <rlatpdms1014@gmail.com>
2024-04-22 00:32:37 +0900

    feat 게시글 저장, 불러오기, 삭제, 수정을 위한 Post 서비스 생성

--- src/main/java/org/jenga/dantong/post/PostService.java
+++ src/main/java/org/jenga/dantong/post/PostService.java
@@ -1,9 +1,12 @@
 package org.jenga.dantong.post;
 
 import org.jenga.dantong.post.model.entity.Post;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jenga.dantong.post.model.entity.PostResponseDto;
+import org.jenga.dantong.post.model.entity.PostSaveDto;
+import org.jenga.dantong.post.model.entity.PostUpdateDto;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,10 +18,37 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-    public int savePost(Post post){
+    public int savePost(PostSaveDto postSaveDto){
+        Post post = Post.builder()
+                .userId(postSaveDto.getUserId())
+                .title(postSaveDto.getTitle())
+                .description(postSaveDto.getDescription())
+                .content(postSaveDto.getContent())
+                .date(postSaveDto.getDate())
+                .shown(postSaveDto.isShown())
+                .build();
+
+        System.out.println("**** Post Info ****");
+        System.out.println("Title: " + postSaveDto.getTitle());
+        System.out.println("Description: " + postSaveDto.getDescription());
+        System.out.println("Content: " +postSaveDto.getContent());
+        System.out.println("*******************");
+
         postRepository.save(post);
 
         return post.getPostId();
+    }
+
+    public PostResponseDto findPost(int postId){
+        Post post = postRepository.findByPostId(postId);
+
+        return PostResponseDto.builder()
+                .userId(post.getUserId())
+                .title(post.getTitle())
+                .description(post.getDescription())
+                .content(post.getContent())
+                .date(post.getDate())
+                .build();
     }
 
     public int deletePost(int postId){
@@ -29,18 +59,41 @@ public class PostService {
         return postId;
     }
 
-    public List<Post> showPost() {
-        List<Post> posts = postRepository.findRemainPost();
+    public List<PostResponseDto> showAllPost() {
+        List<Post> posts = postRepository.findByShownTrue();
+        List<PostResponseDto> postResponseDtos = new ArrayList<>();
 
-        return posts;
+        for (Post currPost : posts) {
+            postResponseDtos.add(
+                    PostResponseDto.builder()
+                            .userId(currPost.getUserId())
+                            .title(currPost.getTitle())
+                            .content(currPost.getContent())
+                            .description(currPost.getDescription())
+                            .date(currPost.getDate())
+                            .build()
+            );
+        }
+
+        return postResponseDtos;
     }
 
-    public Post updatePost(int postId, String content) {
-        Post post = postRepository.findByPostId(postId);
+    public int updatePost(PostUpdateDto updatedPost) {
+        Post post = postRepository.findByPostId(updatedPost.getPostId());
 
-        post.setContent(content);
+        post.setContent(updatedPost.getContent());
+        post.setTitle(updatedPost.getTitle());
+        post.setDescription(updatedPost.getDescription());
+        post.setDate(updatedPost.getUpdateDate());
+
+        System.out.println("**** Post Info ****");
+        System.out.println("Title: " + post.getTitle());
+        System.out.println("Description: " + post.getDescription());
+        System.out.println("Content: " + post.getContent());
+        System.out.println("*******************");
+
         postRepository.save(post);
 
-        return post;
+        return post.getPostId();
     }
 }
