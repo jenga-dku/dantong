ea68e253d60583507aad06f1930b0813d199db39
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-04-22 23:26:56 +0900

    fix: @RestController 추가

--- src/main/java/org/jenga/dantong/user/controller/EmailController.java
+++ src/main/java/org/jenga/dantong/user/controller/EmailController.java
@@ -7,9 +7,11 @@ import org.jenga.dantong.user.service.EmailService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/user/email")
 @RequiredArgsConstructor
+@RestController
 public class EmailController {
 
     private final EmailService emailService;
