a6f711f872d06563eaab0e6160b90daec788a2dd
Sermadl <rlatpdms1014@gmail.com>
2024-05-03 22:25:24 +0900

    feat: 설문 Request 추가

---
+++ src/main/java/org/jenga/dantong/survey/model/dto/SurveyIdInfoRequest.java
@@ -0,0 +1,12 @@
+package org.jenga.dantong.survey.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class SurveyIdInfoRequest {
+
+    private int surveyId;
+}
---
+++ src/main/java/org/jenga/dantong/survey/model/dto/SurveyItemIdInfoRequest.java
@@ -0,0 +1,13 @@
+package org.jenga.dantong.survey.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class SurveyItemIdInfoRequest {
+
+    private int surveyItemId;
+}
