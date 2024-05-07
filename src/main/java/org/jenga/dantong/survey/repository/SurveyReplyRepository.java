9d873e6bc3c664fd3b880a36936399c1bcf404e5
Sermadl <rlatpdms1014@gmail.com>
2024-05-08 03:19:10 +0900

    style: Repository에 빈 주입

--- src/main/java/org/jenga/dantong/survey/repository/SurveyReplyRepository.java
+++ src/main/java/org/jenga/dantong/survey/repository/SurveyReplyRepository.java
@@ -2,6 +2,8 @@ package org.jenga.dantong.survey.repository;
 
 import org.jenga.dantong.survey.model.entity.SurveyReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SurveyReplyRepository extends JpaRepository<SurveyReply, Long> {
 }
--- src/main/java/org/jenga/dantong/survey/repository/SurveyRepository.java
+++ src/main/java/org/jenga/dantong/survey/repository/SurveyRepository.java
@@ -2,7 +2,9 @@ package org.jenga.dantong.survey.repository;
 
 import org.jenga.dantong.survey.model.entity.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     Survey findBySurveyId(int surveyId);
 }
