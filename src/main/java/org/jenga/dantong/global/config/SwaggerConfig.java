8237dcb9e5f385efe0f6974a7c223afff75b961e
Sermadl <rlatpdms1014@gmail.com>
2024-05-28 18:03:13 +0900

    feature: Swagger JWT 인증 토큰 저장 기능 구현

--- src/main/java/org/jenga/dantong/global/config/SwaggerConfig.java
+++ src/main/java/org/jenga/dantong/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package org.jenga.dantong.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -9,18 +13,63 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "Dantong App", version = "v01"))
+@SecurityScheme(
+        name = "JWT Token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/**"};
+    public OpenAPI getOpenApi() {
 
-        return GroupedOpenApi.builder()
-                .group("Dantong OPEN API v1")
-                .pathsToMatch(paths)
-                .build();
+        return new OpenAPI().components(new Components());
     }
 
+    @Bean
+    public GroupedOpenApi getUserApi() {
+
+        return GroupedOpenApi
+                .builder()
+                .group("user & email")
+                .pathsToMatch("/user/**")
+                .build();
+
+    }
+
+    @Bean
+    public GroupedOpenApi getSurveyApi() {
+
+        return GroupedOpenApi
+                .builder()
+                .group("survey")
+                .pathsToMatch("/survey/**")
+                .build();
+
+    }
+
+    @Bean
+    public GroupedOpenApi getPostApi() {
+
+        return GroupedOpenApi
+                .builder()
+                .group("post")
+                .pathsToMatch("/post/**")
+                .build();
+
+    }
+
+    @Bean
+    public GroupedOpenApi getAllApi() {
+
+        return GroupedOpenApi
+                .builder()
+                .group("All")
+                .pathsToMatch("/**")
+                .build();
+
+    }
 }
--- src/main/java/org/jenga/dantong/post/controller/PostController.java
+++ src/main/java/org/jenga/dantong/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package org.jenga.dantong.post.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jenga.dantong.global.auth.jwt.AppAuthentication;
@@ -13,8 +14,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Slf4j
 @RequestMapping("/post")
 @RestController
@@ -25,6 +24,7 @@ public class PostController {
 
     @PostMapping()
     @UserAuth
+    @SecurityRequirement(name = "JWT Token")
     public void post(@RequestBody PostCreateRequest postSaveRequest, AppAuthentication auth)
             throws Exception {
 
@@ -41,6 +41,7 @@ public class PostController {
 
     @PatchMapping()
     @UserAuth
+    @SecurityRequirement(name = "JWT Token")
     public void edit(@RequestBody PostUpdateRequest post, AppAuthentication auth) {
 
         postService.updatePost(post, auth.getUserId());
@@ -48,7 +49,7 @@ public class PostController {
 
     @GetMapping("/list")
     public ResponseEntity<Page<PostResponse>> list(
-        @RequestParam(required = false) Category category) {
+            @RequestParam(required = false) Category category) {
 
 
         Page<PostResponse> posts;
