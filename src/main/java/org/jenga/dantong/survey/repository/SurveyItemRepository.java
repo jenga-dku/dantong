3062fe06f9af26dd8802d122decf9fc8defba8f6
Sermadl <rlatpdms1014@gmail.com>
2024-06-03 15:41:19 +0900

    fix: repository JPA 함수 이름 변경

--- src/main/java/org/jenga/dantong/survey/repository/SurveyItemRepository.java
+++ src/main/java/org/jenga/dantong/survey/repository/SurveyItemRepository.java
@@ -6,14 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SurveyItemRepository extends JpaRepository<SurveyItem, Long> {
 
     List<SurveyItem> findBySurvey(Survey survey);
 
-    Optional<SurveyItem> findBySurvey_SurveyIdAndSurveyItemId(Long surveyId, Long surveyItemId);
-
-    List<SurveyItem> findBySurvey_SurveyIdAndShownTrue(Long surveyId);
+    List<SurveyItem> findBySurveyAndShownTrue(Survey survey);
 }
