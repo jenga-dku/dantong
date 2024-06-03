3483919674086878ab5f625280992bcb8f9f2cf1
Sermadl <rlatpdms1014@gmail.com>
2024-06-03 19:20:53 +0900

    feature: Survey Item 설명 입력 기능 추가

--- src/main/java/org/jenga/dantong/survey/model/dto/SurveyItemCreateRequest.java
+++ src/main/java/org/jenga/dantong/survey/model/dto/SurveyItemCreateRequest.java
@@ -1,10 +1,11 @@
 package org.jenga.dantong.survey.model.dto;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jenga.dantong.survey.model.entity.Tag;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -13,5 +14,6 @@ public class SurveyItemCreateRequest {
 
     private String title;
     private Tag tag;
+    private String description;
     private List<String> options;
 }
