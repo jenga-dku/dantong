68e7de39d70d2c208440f0ab50d63533795d38bf
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-06-04 19:10:56 +0900

    fix: 시작 및 종료 시각 필수 입력 validation

--- src/main/java/org/jenga/dantong/post/model/dto/PostCreateRequest.java
+++ src/main/java/org/jenga/dantong/post/model/dto/PostCreateRequest.java
@@ -1,5 +1,6 @@
 package org.jenga.dantong.post.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Getter;
@@ -22,9 +23,10 @@ public class PostCreateRequest {
     private Category category;
     private List<MultipartFile> imageFiles;
 
+    @NotBlank(message = "시작 시각은 필수 입력값입니다")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startTime;
-
+    @NotBlank(message = "종료 시각은 필수 입력값입니다")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endTime;
 
