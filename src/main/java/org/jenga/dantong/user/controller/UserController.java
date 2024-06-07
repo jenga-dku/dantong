faeb33cc155b0189d9d0ca5634d5dfbd7da43680
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-06-08 03:11:59 +0900

    fix: userAuth api에 security requirements 제거

--- src/main/java/org/jenga/dantong/user/controller/UserController.java
+++ src/main/java/org/jenga/dantong/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package org.jenga.dantong.user.controller;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.jenga.dantong.global.auth.jwt.AppAuthentication;
@@ -12,7 +11,12 @@ import org.jenga.dantong.user.model.dto.UserResponse;
 import org.jenga.dantong.user.model.entity.User;
 import org.jenga.dantong.user.service.UserSignupService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -22,7 +26,6 @@ public class UserController {
     private final UserSignupService userSignupService;
 
     @UserAuth
-    @SecurityRequirement(name = "JWT Token")
     @GetMapping
     public UserResponse info(AppAuthentication authentication) {
         return userSignupService.userInfo(authentication.getUserId());
@@ -37,7 +40,7 @@ public class UserController {
 
     @PostMapping(path = "/signup/{signup-token}")
     public void signup(@Valid @RequestBody SignupRequest request,
-                       @PathVariable("signup-token") String signupToken) {
+        @PathVariable("signup-token") String signupToken) {
         User user = userSignupService.signup(request, signupToken);
     }
 }
