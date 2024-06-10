c60ed9843f6ca107b1a5431c2847159b2d7ad607
Sermadl <rlatpdms1014@gmail.com>
2024-06-11 01:07:38 +0900

    feature: 사용자 개인정보 수정 기능 구현 및 request enum 검증 기능 구현

--- src/main/java/org/jenga/dantong/user/controller/UserController.java
+++ src/main/java/org/jenga/dantong/user/controller/UserController.java
@@ -1,46 +1,51 @@
 package org.jenga.dantong.user.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jenga.dantong.global.auth.jwt.AppAuthentication;
 import org.jenga.dantong.global.base.UserAuth;
 import org.jenga.dantong.user.model.dto.request.LoginRequest;
 import org.jenga.dantong.user.model.dto.request.SignupRequest;
+import org.jenga.dantong.user.model.dto.request.UserInfoEditRequest;
 import org.jenga.dantong.user.model.dto.response.LoginResponse;
 import org.jenga.dantong.user.model.dto.response.UserResponse;
 import org.jenga.dantong.user.model.entity.User;
 import org.jenga.dantong.user.service.UserSignupService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserSignupService userSignupService;
 
-    @UserAuth
     @GetMapping
-    public UserResponse info(AppAuthentication authentication) {
-        return userSignupService.userInfo(authentication.getUserId());
+    @UserAuth
+    public UserResponse info(AppAuthentication auth) {
+        return userSignupService.userInfo(auth.getUserId());
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Validated LoginRequest loginRequest) {
         LoginResponse loginResponse = userSignupService.login(loginRequest);
 
         return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping(path = "/signup/{signup-token}")
-    public void signup(@Valid @RequestBody SignupRequest request,
-        @PathVariable("signup-token") String signupToken) {
+    public void signup(@RequestBody @Validated SignupRequest request,
+                       @PathVariable("signup-token") String signupToken) {
         User user = userSignupService.signup(request, signupToken);
+    }
+
+    @PatchMapping(path = "/edit")
+    @UserAuth
+    public void edit(@RequestBody @Validated UserInfoEditRequest request,
+                     AppAuthentication auth) {
+        userSignupService.userInfoEdit(request, auth.getUserId());
     }
 }
--- src/main/java/org/jenga/dantong/user/model/dto/request/SignupRequest.java
+++ src/main/java/org/jenga/dantong/user/model/dto/request/SignupRequest.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jenga.dantong.user.model.entity.Major;
+import org.jenga.dantong.user.model.entity.validator.Enum;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,8 +20,15 @@ public class SignupRequest {
     @NotBlank(message = "비밀번호는 필수 입력값입니다")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력값입니다")
     private String name;
+
+    @NotBlank(message = "전화번호는 필수 입력값입니다")
+    @Pattern(regexp = "^010[0-9]{4}[0-9]{4}$", message = "전화번호를 정확히 입력해주세요")
     private String phoneNumber;
+
+    @Enum(enumClass = Major.class, message = "전공을 선택해주세요")
     private Major major;
 
 }
