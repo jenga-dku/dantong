5006676580e3159d5788fc2a5f1203b938e24e62
Sermadl <rlatpdms1014@gmail.com>
2024-07-17 21:15:32 +0900

    feature: 친구 요청/수락/조회 기능 추가

---
+++ src/main/java/org/jenga/dantong/friend/controller/FriendController.java
@@ -0,0 +1,48 @@
+package org.jenga.dantong.friend.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jenga.dantong.friend.model.dto.response.FriendListResponse;
+import org.jenga.dantong.friend.model.dto.response.RequestListResponse;
+import org.jenga.dantong.friend.service.FriendService;
+import org.jenga.dantong.global.auth.jwt.AppAuthentication;
+import org.jenga.dantong.global.base.UserAuth;
+import org.jenga.dantong.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequestMapping("/friend")
+@RestController
+@RequiredArgsConstructor
+public class FriendController {
+    private final UserRepository userRepository;
+    private final FriendService friendService;
+
+    @UserAuth
+    @PostMapping("/send/{studentId}")
+    public void sendRequest(@Valid @PathVariable("studentId") String studentId, AppAuthentication auth) {
+        friendService.sendRequest(studentId, auth.getUserId());
+    }
+
+    @UserAuth
+    @GetMapping("/request")
+    public ResponseEntity<Page<RequestListResponse>> getRequestList(AppAuthentication auth, Pageable pageable) {
+        return ResponseEntity.ok(friendService.getRequestList(auth.getUserId(), pageable));
+    }
+
+    @UserAuth
+    @PostMapping("/accept/{friendId}")
+    public void acceptRequeest(@PathVariable("friendId") Long friendId, AppAuthentication auth) {
+        friendService.acceptRequest(friendId, auth.getUserId());
+    }
+
+    @UserAuth
+    @GetMapping("/list")
+    public ResponseEntity<Page<FriendListResponse>> getFriendList(AppAuthentication auth, Pageable pageable) {
+        return ResponseEntity.ok(friendService.getFriendList(auth.getUserId(), pageable));
+    }
+}
