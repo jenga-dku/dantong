5b3faf477e32c13b5b412625bb034e737736b029
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-07-23 16:52:29 +0900

    refactor: api url 변경 및 swagger->springdoc으로 변경

--- src/main/java/org/jenga/dantong/survey/controller/SurveyController.java
+++ src/main/java/org/jenga/dantong/survey/controller/SurveyController.java
@@ -1,5 +1,7 @@
 package org.jenga.dantong.survey.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jenga.dantong.global.auth.jwt.AppAuthentication;
@@ -16,9 +18,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequestMapping("/survey")
@@ -29,6 +36,7 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @GetMapping("/{surveyId}")
+    @Operation(summary = "설문 조회", description = "설문번호로 설문 조회")
     public ResponseEntity<SurveyResponse> findSurvey(@PathVariable("surveyId") Long surveyId) {
         SurveyResponse response = surveyService.findSurvey(surveyId);
 
@@ -37,8 +45,9 @@ public class SurveyController {
 
     @PostMapping()
     @UserAuth
+    @Operation(summary = "설문 생성", description = "token으로 권한 확인 후 설문 생성")
     public ResponseEntity<SurveyIdInfoResponse> create(
-            @RequestBody @Validated SurveyCreateRequest survey, AppAuthentication auth) {
+        @RequestBody @Validated SurveyCreateRequest survey, AppAuthentication auth) {
 
         survey.itemOptionCheck();
         Long surveyId = surveyService.create(survey, auth.getUserId());
@@ -47,10 +56,11 @@ public class SurveyController {
     }
 
     @PatchMapping("/{surveyId}")
+    @Operation(summary = "설문 수정", description = "token과 설문 번호로 권한 확인 후 설문 수정")
     @UserAuth
     public ResponseEntity<SurveyIdInfoResponse> update(@PathVariable("surveyId") Long id,
-                                                       @RequestBody @Validated SurveyUpdateRequest survey,
-                                                       AppAuthentication auth) {
+        @RequestBody @Validated SurveyUpdateRequest survey,
+        AppAuthentication auth) {
 
         survey.itemOptionCheck();
         Long surveyId = surveyService.updateSurvey(id, survey, auth.getUserId());
@@ -59,6 +69,7 @@ public class SurveyController {
     }
 
     @DeleteMapping("/{surveyId}")
+    @Operation(summary = "설문 삭제", description = "token과 설문 번호로 권한 확인 후 설문 삭제")
     @UserAuth
     public void deleteSurvey(@PathVariable("surveyId") Long surveyId, AppAuthentication auth) {
 
@@ -66,23 +77,26 @@ public class SurveyController {
     }
 
     @DeleteMapping("/{surveyId}/{surveyItemId}")
+    @Operation(summary = "설문 문항 삭제", description = "token으로 권한 확인 후 설문,설문 문항 번호로 설문 삭제")
     @UserAuth
     public void deleteSurveyItem(@PathVariable("surveyId") Long surveyId,
-                                 @PathVariable("surveyItemId") Long surveyItemId,
-                                 AppAuthentication auth) {
+        @PathVariable("surveyItemId") Long surveyItemId,
+        AppAuthentication auth) {
 
         surveyService.deleteSurveyItem(surveyId, surveyItemId, auth.getUserId());
     }
 
     @GetMapping("/admin/surveys")
     @AdminAuth
+    @Operation(summary = "설문 전체 조회", description = "설문 정보 전체 조회")
     public ResponseEntity<Page<SurveyAdminResponse>> getSurveyInfos(Pageable pageable) {
         Page<SurveyAdminResponse> surveyInfos = surveyService.getSurveyInfos(pageable);
         return ResponseEntity.ok(surveyInfos);
     }
 
-    @GetMapping("/ticket")
+    @GetMapping("/my/ticket")
     @UserAuth
+    @Operation(summary = "내 티켓 조회", description = "내 티켓 조회")
     public ResponseEntity<List<TicketResponse>> tickets(AppAuthentication auth) {
         List<TicketResponse> tickets = surveyService.getTickets(auth.getUserId());
         return ResponseEntity.ok(tickets);
