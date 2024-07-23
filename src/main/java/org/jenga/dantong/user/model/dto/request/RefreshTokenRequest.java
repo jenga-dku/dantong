b107ebd8b1e182a35a129fc9fb1d231869d56684
dku19jam <70689205+dku19jam@users.noreply.github.com>
2024-07-23 19:15:36 +0900

    feat: token reissue 기능

---
+++ src/main/java/org/jenga/dantong/user/model/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,11 @@
+package org.jenga.dantong.user.model.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RefreshTokenRequest {
+
+    private String refreshToken;
+}
---
+++ src/main/java/org/jenga/dantong/user/service/MyService.java
@@ -0,0 +1,11 @@
+package org.jenga.dantong.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MyService {
+
+
+}
