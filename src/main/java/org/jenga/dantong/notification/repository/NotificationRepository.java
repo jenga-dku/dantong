c4d31c4434ee124090368deba26fca471a0c6572
Sermadl <rlatpdms1014@gmail.com>
2024-08-01 10:25:53 +0900

    feature: 알림 기능 추가

---
+++ src/main/java/org/jenga/dantong/notification/repository/NotificationRepository.java
@@ -0,0 +1,30 @@
+package org.jenga.dantong.notification.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.jenga.dantong.user.model.dto.request.LoginRequest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class NotificationRepository {
+
+    private final StringRedisTemplate tokenRedisTemplate;
+
+    public void saveToken(LoginRequest loginRequest) {
+        tokenRedisTemplate.opsForValue()
+                .set(loginRequest.getStudentId(), loginRequest.getStudentId());
+    }
+
+    public String getToken(String studentId) {
+        return tokenRedisTemplate.opsForValue().get(studentId);
+    }
+
+    public void deleteToken(String studentId) {
+        tokenRedisTemplate.delete(studentId);
+    }
+
+    public boolean hasKey(String studentId) {
+        return tokenRedisTemplate.hasKey(studentId);
+    }
+}
